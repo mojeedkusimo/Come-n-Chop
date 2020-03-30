f203ecbddd4952d195a4ab1616cd6b89807ed5c8
Mojeedkush <mkusimo90@gmail.com>
2020-03-30 13:44:17 +0100

    trying out some text aniimations

---
+++ UI/assets/js/script.js
@@ -0,0 +1,23 @@
+let addMsg = ['Unexpected guests?', 'Cooking gone wrong?', 'Movie Marathon?', 'Game Night?', 'Late Night at Office?', 'Hungry?'];
+
+let speed = 80;
+let j = 0;
+
+function clear () {
+    setTimeout(function(){ document.getElementById("title").innerHTML = ''; }, 3000);
+
+}
+
+function type() {
+  if (j < addMsg[1].length) {
+    document.getElementById("title").innerHTML += addMsg[1].charAt(j);
+    j++;
+    setTimeout(type, speed);
+  }
+  clear();
+}
+
+
+
+type();
+
